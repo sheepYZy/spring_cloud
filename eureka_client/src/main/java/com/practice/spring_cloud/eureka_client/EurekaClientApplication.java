0d5eceae7e8804b8ec7fa89dc4262104a7e2addc
杨振宇 <zhenyu.yang@hand-china.com>
2018-11-27 21:04:36 +0800

    add:spring cloud demo

---
+++ eureka_client/src/main/java/com/practice/spring_cloud/eureka_client/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.practice.spring_cloud.eureka_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(
+                EurekaClientApplication.class)
+                .web(true).run(args);
+    }
+}
