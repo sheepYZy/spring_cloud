afafea78c509a10cfa93e4bc3d2a23931afa402a
杨振宇 <zhenyu.yang@hand-china.com>
2018-12-16 18:23:38 +0800

    add:add springcloud eureka consumer ribbon

---
+++ eureka_consumer_ribbon/src/main/java/com/practice/spring_cloud/eureka_consumer_ribbon/DcController.java
@@ -0,0 +1,27 @@
+package com.practice.spring_cloud.eureka_consumer_ribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author zhenyu.yang@hand-china.com(杨振宇)
+ * @version 1.0
+ * @description
+ * @name DcController
+ * @date 2018/11/27
+ */
+@RestController
+public class DcController {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);
+    }
+
+}
